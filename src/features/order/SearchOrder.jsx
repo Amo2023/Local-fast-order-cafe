c30e9b646ffd621b5bd82ae1e5fc37349f9b09ed
Amo C <amochen429@qq.com>
2024-10-14 09:21:41 +1000

    Add SearchOrcer.jsx

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    console.log("test");
+    navigate(`/order/${query}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="search order num #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchOrder;
