6d75527835d91018d4446628c6b63efe24e2329c
Amo C <amochen429@qq.com>
2024-10-14 10:19:24 +1000

    Set up Tailwind css and tailwind prettier

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder"
 
 function Header() {
     return (
-        <header>
+        <header className="bg-yellow-400">
             <Link to="/">Fast React Pizza Co.</Link>
             <SearchOrder/>
         </header>
