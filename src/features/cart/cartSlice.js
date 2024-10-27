2541b7d9d3a8aa5908768e194cc8d25614f5679b
Amo C <amochen429@qq.com>
2024-10-27 16:38:06 +1000

    Added getCurrentQuantityById helper in cart Slice to toggle add/delete button in MenuItem component

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    cart: [],
+  cart: [],
   // cart: [
   //   {
   //     pizzaId: 12,
@@ -51,11 +51,14 @@ export const {
 
 export default cartSlice.reducer;
 
-export const getCart=state=>state.cart.cart
+export const getCart = (state) => state.cart.cart;
 
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => (sum += item.quantity), 0);
 
 export const getTotalCartPrice = (state) =>
-  state.cart.cart.reduce((sum, item) => (sum += item.totalPrice),0);
+  state.cart.cart.reduce((sum, item) => (sum += item.totalPrice), 0);
 
+//if currentQuantity=null/undefined,return 0
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,25 +1,23 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from "../cart/cartSlice";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantify = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantify > 0;
 
   function handleAddToCart() {
-
     const newItem = {
-      // cart: [
-        // {
-          pizzaId: id,
-          name,
-          quantity: 1,
-          unitPrice,
-          totalPrice: unitPrice * 1,
-        }
-    //   // ],
-    // };
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
     dispatch(addItem(newItem));
   }
 
@@ -46,11 +44,13 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {!soldOut && (
+          {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart&&
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
-          )}
+          }
         </div>
       </div>
     </li>
